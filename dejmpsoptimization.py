b64ebe38fa83ace0a480eb7bfd42b7535702464c
jperezno <arbolnoriega@gmail.com>
2025-01-15 11:06:33 +0100

    Corrected errors, works as expected now

--- dejmpsoptimization.py
+++ dejmpsoptimization.py
@@ -10,24 +10,36 @@ def equ7lin(y1,y2,N):
     line= (2*y1*y2)/N
     return (line)
 
+def nor (A,B,C,D):
+    norm=((A+B)**2+(C+D)**2)
+    return(norm)
+
 #lets give some numbers to the program
 #first round here we assume A remains as it is but take B=C=D
 
 A_1=0.95 #lets assume A is just a random number close to 1
 print('The value of A wont change as its fixed A=',A_1, 'and the sum of A,B,C,D cant be more than 1')
-B=float(input('What is the value of B=C=D:'))#here it will be 0.01666
+# B=float(input('What is the value of B=C=D:'))#here it will be 0.01666
+B=(1-A_1)/3
 C=B
 D=B
-Nor=((A_1+B)**2+(C+D)**2)
+Nor=nor(A_1,B,C,D)
 print('The original fidelity is F=',Nor)
 i=1
+iter=10
+#make this a function 
 while i <= iter:
     #inside the brackets we modify ABCD accordingly, N remains unchanged
     Asign=equ7quad(A_1,B,Nor) #this one for A
+    #paper B &D aere flipped change
     Bsign=equ7lin(C,D,Nor)
     Csign=equ7quad(C,D,Nor)
     Dsign=equ7lin(A_1,B,Nor)
     print('Table:')
     print(i,Asign,Bsign,Csign,Dsign)
-
-    
+    i=i+1
+    A_1=Asign
+    B=Bsign
+    C=Csign
+    D=Dsign
+    Nor=nor(A_1,B,C,D)  
